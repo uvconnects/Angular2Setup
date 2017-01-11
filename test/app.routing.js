c7e6f5d7c9b7fd36aa79c0f205a1e6f5a145d962
anthony.price <anthony.price@fairviewinvest.com>
2017-01-11 09:32:16 -0500

    Initial commit

--- test/app.routing.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var router_1 = require('@angular/router');
-var appRoutes = [
-    { path: '', redirectTo: 'index', pathMatch: 'full' }
-];
-exports.routing = router_1.RouterModule.forRoot(appRoutes);
-//# sourceMappingURL=app.routing.js.map
