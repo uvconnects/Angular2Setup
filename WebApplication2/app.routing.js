2012d28e60ab96458d7ee36d6e54a0abe91800a9
anthony.price <anthony.price@FIS-ANTHONY>
2017-01-11 08:57:42 -0500

    Add project files.

---
+++ WebApplication2/app.routing.js
@@ -0,0 +1,7 @@
+"use strict";
+var router_1 = require("@angular/router");
+var appRoutes = [
+    { path: '', redirectTo: 'index', pathMatch: 'full' }
+];
+exports.routing = router_1.RouterModule.forRoot(appRoutes);
+//# sourceMappingURL=app.routing.js.map
